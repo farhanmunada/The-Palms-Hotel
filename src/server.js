04311fb5cd57d6001277ef6f2bd97cedcbf67d38
farhanmunada <farchanang@gmail.com>
2025-01-16 22:46:38 +0700

    edit PORT

--- src/server.js
+++ src/server.js
@@ -8,7 +8,8 @@ const loginRoute = require('./routes/loginRoute');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const port = 5000;
+
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
